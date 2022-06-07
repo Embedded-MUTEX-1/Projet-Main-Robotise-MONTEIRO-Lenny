937593480c83d6214570ccdc00401c61a5017eb5
lenny1411 <lenny.monteiro59@gmail.com>
2022-06-07 10:04:24 +0200

    Update main.c
    Ajout des "events" et fonctions permettant à l'ESP32 de se connecter au réseau wifi du routeur cisco

--- Main_Robotisee/src/main.c
+++ Main_Robotisee/src/main.c
@@ -1,7 +1,13 @@
 #include <driver/i2c.h>
 #include <esp_log.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
 #include <math.h>
@@ -45,20 +51,133 @@
 #define ACK_VAL         0x0     /*!< I2C ack value */
 #define NACK_VAL        0x1     /*!< I2C nack value */
 
-static char tag[] = "PCA9685";
-static const char *TAG = "Angle :";
-
 #undef ESP_ERROR_CHECK
 #define ESP_ERROR_CHECK(x)   do { esp_err_t rc = (x); if (rc != ESP_OK) { ESP_LOGE("err", "esp_err_t = %d", rc); assert(0 && #x);} } while(0);
 
-static void i2c_example_master_init(void);
+#define LED GPIO_NUM_18
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static int s_retry_num = 0; //nombre d'essai de reconnexion au reseau WIFI
+
+//fonction gérant les évènements WIFI
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_CONNECTED)
+    {
+        ESP_LOGI("Wifi", "Wifi connecté.");
+        gpio_set_level(LED, 1);
+    }
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 3) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI("Wifi", "Wifi deconnecté. Tentative de reconnection");
+            gpio_set_level(LED, 0);
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI("Wifi","connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI("Wifi", "IP:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+//fonction pour se connecter au wifi
+void wifi_init()
+{
+    // Initialisation
+    s_wifi_event_group = xEventGroupCreate();
+
+    esp_netif_init();
+
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    // enregistrement des évènements
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    esp_event_handler_instance_register(WIFI_EVENT,
+                                                    ESP_EVENT_ANY_ID,
+                                                    &event_handler,
+                                                    NULL,
+                                                    &instance_any_id);
+    esp_event_handler_instance_register(IP_EVENT,
+                                                    IP_EVENT_STA_GOT_IP,
+                                                    &event_handler,
+                                                    NULL,
+                                                     &instance_got_ip);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "Routeur Main Robotisee",
+            .password = "12345678",
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+
+    ESP_LOGI("Wifi", "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI("wifi", "connected to ap SSID:%s password:%s",
+                 "Routeur Main Robotisee", "12345678");
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI("Wifi", "Failed to connect to SSID:%s, password:%s",
+                "Routeur Main Robotisee", "12345678");
+    } else {
+        ESP_LOGE("Wifi", "UNEXPECTED EVENT");
+    }
+}
+
+//initialisation de la memoire flash
+void NVS_Init()
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+
+}
 
 /**
  * @brief i2c master initialization
  */
 static void i2c_example_master_init(void)
 {
-    ESP_LOGD(tag, ">> PCA9685");
+    ESP_LOGD("i2c", "init");
     i2c_config_t conf = {
     .mode = I2C_MODE_MASTER,
     .sda_io_num = GPIO_NUM_21,         // select GPIO specific to your project
@@ -87,7 +206,12 @@ void app_main()
     setFrequencyPCA9685(50);  // fréquence PWM a 50Hz
     turnAllOff();//met toutes les consigne à zero
 
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
     printf("Finished setup, entering loop now\n");
+
+    NVS_Init();
+    wifi_init();
 
     while(1)
     { 
